b4ab2f472aef0b7758b22dc6ec745a586793a012
KANG <526722199@qq.com>
2021-02-27 21:18:49 +0800

    测试代码

---
+++ src/com/company/demo.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author KANG
+ * @create 2021-02-27 21:15
+ **/
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("测试代码");
+    }
+}
